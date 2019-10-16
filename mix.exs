37752fae585278b89dd9c297b644b933676f8158
Thiago Lima <thiago.lima@radsquare.ai>
2019-10-16 11:22:49 -0300

    Add project dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,12 @@ defmodule Herald.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:uuid, "~> 1.1"},
+      {:ecto, "~> 3.2"},
+      {:jason, "~> 1.1"},
+
+      # Development dependencies
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 end
