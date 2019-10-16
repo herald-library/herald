063ff99b17030d9e66cddbe90069098795ccf22c
Thiago Lima <thiago.lima@radsquare.ai>
2019-10-16 11:35:42 -0300

    Implement Herald.Message.new/3

--- lib/herald/message.ex
+++ lib/herald/message.ex
@@ -1,4 +1,18 @@
 defmodule Herald.Message do
+  @derive Jason.Encoder
+  defstruct [
+    id:      nil,
+    queue:   nil,
+    errors:  [],
+    payload: nil
+  ]
+
+  @type t :: %__MODULE__{
+    id:      UUID.t(),
+    queue:   binary(),
+    payload: map()
+  }
+
   @doc false
   defmacro __using__(_opts) do
     quote do
@@ -24,6 +38,27 @@ defmodule Herald.Message do
       end
 
       @schema Map.put(@schema, name, type)
+    end
+  end
+
+  @doc """
+  Create new message and validate their payload
+  """
+  @spec new(binary(), map(), any()) :: t()
+  def new(queue, payload, opts \\ []) do
+    %__MODULE__{}
+    |> set_message_id(opts)
+    |> Map.put(:queue, queue)
+    |> Map.put(:payload, payload)
+  end
+
+  defp set_message_id(%__MODULE__{} = message, opts) do
+    Keyword.get(opts, :id)
+    |> is_nil()
+    |> if do
+      Map.put(message, :id, UUID.uuid4())
+    else
+      Map.put(message, :id, Keyword.get(opts, :id))
     end
   end
 end
