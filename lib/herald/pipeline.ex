d0f861244db48a9b00d8fa18f56c377f0e707bf2
Thiago Lima <thiago.lima@radsquare.ai>
2019-12-10 21:55:37 -0300

    Improve Pipeline documentation

--- lib/herald/pipeline.ex
+++ lib/herald/pipeline.ex
@@ -1,6 +1,14 @@
 defmodule Herald.Pipeline do
   @moduledoc """
   Pipeline is where messages are processed.
+
+  All message processing is started by function `run/3`. 
+
+  When `run/3` receives a message, it runs the following steps:
+  * *pre_processing* - Will convert the message to a struct, using 
+  the schema defined in route for the given queue;
+  * *processor_caler* - Will call the `processor` defined in router for
+  the given queue.
   """
 
   alias Herald.Errors.MissingRouter
@@ -12,7 +20,19 @@ defmodule Herald.Pipeline do
     :caller_result
   ]
 
-  @doc "Runs the pipeline for a message"
+  @doc """
+  Process a given message.
+
+  Receive the following arguments:
+  * `queue` - The queue where message was received from;
+  * `message` - The received message in a raw 
+  state (i.e. the String as received from broker)
+
+  *Warning:* When you call this function without
+  [configure a router](Herald.Router.html) for you
+  application, it will raises the exception
+  `Herald.Errors.MissingRouter`
+  """
   @spec run(String.t(), String.t(), String.t()) :: :ok | {:error, atom()}
   def run(queue, message, opts \\ []) do
     Application.get_env(:herald, :router)
@@ -54,7 +74,6 @@ defmodule Herald.Pipeline do
     result = 
       message
       |> processor.()
-
 
     Map.put(pipeline, :caller_result, result)
   end
