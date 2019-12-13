5452c63a6cab7c2230604913f4eb3cc2a76e4f62
Thiago Lima <thiago.lima@radsquare.ai>
2019-12-13 17:13:37 -0300

    Add an name in Herald documntation

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Herald.MixProject do
   def project do
     [
       app: :herald,
+      name: "Herald",
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
