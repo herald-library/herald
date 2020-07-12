9a0fa8098eb774cc3b99bea4d1a2e1f5abd2a9c9
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-11 21:12:41 -0300

    Add adapter module to handle adapters protocols

---
+++ lib/herald/adapter.ex
@@ -0,0 +1,30 @@
+defmodule Herald.Adapter do
+  @moduledoc """
+  The adapter base module
+  """
+
+  @adapter_protocol "#{__MODULE__}s"
+  @connection_protocol "#{@adapter_protocol}.Connection"
+  @queue_protocol "#{@adapter_protocol}.Queue"
+
+  @doc """
+  Get implementation module from adapter's module
+  """
+  @spec get_adapter(atom(), module()) :: {:ok, module()} | {:error, binary()}
+  def get_adapter(type, adapter) do
+    module = get_module(type, adapter)
+
+    if Code.ensure_loaded?(module) do
+      {:ok, module}
+    else
+      {:error, "Herald Adapter #{adapter} not found"}
+    end
+  end
+
+  @spec get_module(atom(), module()) :: module()
+  defp get_module(:connection, adapter),
+    do: Module.concat(@connection_protocol, adapter)
+
+  defp get_module(:queue, adapter),
+    do: Module.concat(@queue_protocol, adapter)
+end
